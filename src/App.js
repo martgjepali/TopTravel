e80d9abee474f9e219bb4da11cdbbc7a09be1737
Mart Gjepali <gjepalimart@gmail.com>
2024-07-16 16:14:17 +0200

    fixed signup and signin

--- src/App.js
+++ src/App.js
@@ -9,28 +9,28 @@ import SignUp from "./components/pages/SignUp";
 import Destination from "./components/pages/Destination";
 import ScrollToTop from "./components/ScrollToTop";
 import Packages from "./components/pages/Packages";
+import { AuthProvider } from "./contexts/AuthProvider";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <SearchProvider>
-        <Navbar />
-        <ScrollToTop>
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/services" exact element={<Services />} />
-            <Route path="/sign-in" exact element={<SignIn />} />
-            <Route path="/sign-up" exact element={<SignUp />} />
-            <Route
-              path="/services/activity/:destinationId"
-              element={<Destination />}
-            />
-            <Route path="/filtered-packages/:destinationId" element={<Packages />} />
-            <Route path="/packages/:packageId" element={<Destination />} />
-          </Routes>
-        </ScrollToTop>
-      </SearchProvider>
+      <AuthProvider>
+        <SearchProvider>
+          <Navbar />
+          <ScrollToTop>
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/services" exact element={<Services />} />
+              <Route path="/sign-in" exact element={<SignIn />} />
+              <Route path="/sign-up" exact element={<SignUp />} />
+              <Route path="/services/activity/:destinationId" element={<Destination />} />
+              <Route path="/filtered-packages/:destinationId" element={<Packages />} />
+              <Route path="/packages/:packageId" element={<Destination />} />
+            </Routes>
+          </ScrollToTop>
+        </SearchProvider>
+      </AuthProvider>
     </div>
   );
 }
