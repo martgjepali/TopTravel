a0dd323612a1cda0fe8ababfa28f82addd09008a
Mart Gjepali <gjepalimart@gmail.com>
2024-08-26 18:54:20 +0200

    asdsa

--- src/apis/destinationApi.js
+++ src/apis/destinationApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
+// const API_URL = process.env.REACT_APP_API_URL;
+
+API_URL = "https://server-app-zxcxm.ondigitalocean.app";
 
 export const getDestinations = async ({
   skip = 0,
